f528a2fd49152f32122beeb1ab4e9016d3937b09
radhika2722 <46097340+radhika2722@users.noreply.github.com>
2019-10-20 11:59:52 -0500

    Update final_submission.py

--- final_submission.py
+++ final_submission.py
@@ -177,7 +177,7 @@ for i in range(1,len(g)):
 
 print(path, totalDist)
 final_path = []
-for i in range(2):
+for i in range(50):
     # print(data)
     point_number = path[i]
     next_point_number = path[i+1]
